2c01487b33311a2815934499bcb9571a388e56ba
Daniel Chuks <danieluchechukwuchuks1@gmail.com>
2023-03-20 23:27:48 +0100

    start up

--- index.mjs
+++ index.mjs
@@ -1,11 +1,10 @@
-import {loadStdlib} from '@reach-sh/stdlib';
+import { loadStdlib } from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
 const stdlib = loadStdlib(process.env);
 
 const startingBalance = stdlib.parseCurrency(100);
 
-const [ accAlice, accBob ] =
-  await stdlib.newTestAccounts(2, startingBalance);
+const [accAlice, accBob] = await stdlib.newTestAccounts(2, startingBalance);
 console.log('Hello, Alice and Bob!');
 
 console.log('Launching...');
@@ -14,14 +13,14 @@ const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
 console.log('Starting backends...');
 await Promise.all([
-  backend.Alice(ctcAlice, {
-    ...stdlib.hasRandom,
-    // implement Alice's interact object here
-  }),
-  backend.Bob(ctcBob, {
-    ...stdlib.hasRandom,
-    // implement Bob's interact object here
-  }),
+    backend.Alice(ctcAlice, {
+        ...stdlib.hasRandom,
+        // implement Alice's interact object here
+    }),
+    backend.Bob(ctcBob, {
+        ...stdlib.hasRandom,
+        // implement Bob's interact object here
+    }),
 ]);
 
 console.log('Goodbye, Alice and Bob!');
